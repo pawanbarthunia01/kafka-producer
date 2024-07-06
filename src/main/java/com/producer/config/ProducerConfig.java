47febb80f6006af79cfe459b96a33035c6a82cf3
sunnovation <poojapawandev96@gmail.com>
2024-07-06 22:06:23 +0530

    Modified files

--- src/main/java/com/producer/config/ProducerConfig.java
+++ src/main/java/com/producer/config/ProducerConfig.java
@@ -25,19 +25,19 @@ public class ProducerConfig {
 
 
     @Bean
-    public ProducerFactory<String, Employee> producerFactory(){
+    public ProducerFactory<String, String> producerFactory(){
 
         Map<String,Object> config=new HashMap<>();
         config.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstants.BOOTSTRAP_SERVER);
         config.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
-        return new DefaultKafkaProducerFactory<String, Employee>(config);
+        return new DefaultKafkaProducerFactory<String, String>(config);
     }
 
     @Bean
-    public KafkaTemplate<String, Employee> kafkaTemplate(){
-        return new KafkaTemplate<String, Employee>(producerFactory());
+    public KafkaTemplate<String, String> kafkaTemplate(){
+        return new KafkaTemplate<String, String>(producerFactory());
     }
 
 
--- src/main/java/com/producer/service/ProducerService.java
+++ src/main/java/com/producer/service/ProducerService.java
@@ -1,5 +1,6 @@
 package com.producer.service;
 
+import com.google.gson.Gson;
 import com.producer.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,14 +10,16 @@ import org.springframework.stereotype.Service;
 public class ProducerService {
 
         @Autowired
-        private KafkaTemplate<String,Employee> kafkaTemplate;
+        private KafkaTemplate<String,String> kafkaTemplate;
+
         private final String TOPIC_NAME= "EMPLOYEE_TOPIC";
 
 
 
         public String sendData(Employee employee){
          if(employee.getEmpId()!=0){
-             kafkaTemplate.send(TOPIC_NAME,employee);
+             String json=new Gson().toJson(employee);
+             kafkaTemplate.send(TOPIC_NAME,json);
              return "send Data";
          }else{
              return "Data not send";
